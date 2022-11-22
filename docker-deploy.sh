7cdab96f22bf794c78731e19d69caf67ca001b32
James Staples <james.staples@firemind.com>
2022-11-22 17:32:03 +0000

    password as ARG

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -6,7 +6,7 @@ deployDocker() {
     ECR_URL=$(aws ssm get-parameter --name "/${SERVICE}/${BUILD_STAGE}/ecr_url" | jq -r .Parameter.Value)
     SM_PASSWORD = $(aws secretsmanager get-secret-value --secret-id syndio | jq --raw-output .SecretString | jq -r ."password")
     
-    docker build --build-arg HELLO=SM_PASSWORD -t ${ECR_URL}:latest .
+    docker build --build-arg HELLO=${SM_PASSWORD} -t ${ECR_URL}:latest .
     docker tag ${ECR_URL}:latest ${ECR_URL}:latest
     docker push ${ECR_URL}:latest
 }
